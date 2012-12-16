e983f5cb7ee6e3c0878501663d5d5a2b7188aded
Sean Handley <sean.handley@gmail.com>
2012-12-16 11:30:31 +0100

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BroadbandDowntimeWeb::Application.config.secret_token = '1db7f7125ea15afa80e8a898cf8e2d6217fb0b3df8bd9f20ddebe7da5a3bb44b39dd2f7f2a77066208aeecd7387475698900102319e83e9d5f65264bf8cbce11'
