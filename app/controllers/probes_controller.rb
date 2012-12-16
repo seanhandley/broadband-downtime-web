d7c08bf17016bd89077a2061876d6e8fbd0fabc5
Sean Handley <sean.handley@gmail.com>
2012-12-16 21:57:16 +0100

    Temporary endpoints to test ajax calls.

--- app/controllers/probes_controller.rb
+++ app/controllers/probes_controller.rb
@@ -18,4 +18,12 @@ class ProbesController < ApplicationController
     respond_with(Probe.all)
   end
   
+  def test
+    respond_with([7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6].shuffle)
+  end
+  
+  def months
+    respond_with(['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'])
+  end
+  
 end
