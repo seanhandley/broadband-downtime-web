0ba1ccdf4f22b106971b7c119c5e496d59d99460
Sean Handley <sean.handley@gmail.com>
2012-12-16 23:13:09 +0100

    Tweak test data.

--- app/controllers/probes_controller.rb
+++ app/controllers/probes_controller.rb
@@ -19,11 +19,11 @@ class ProbesController < ApplicationController
   end
   
   def test
-    respond_with([7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6].shuffle)
+    respond_with(5.upto(20.0).to_a.sample(12).shuffle)
   end
   
   def months
-    respond_with(['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'])
+    respond_with(Date::MONTHNAMES.compact)
   end
   
 end
